4b1efcd8b90e880b3da09eb6490495af13d96b3b
tejas <tejasshah@gmail.com>
2023-09-27 19:54:38 +0530

    read request and dispatch

--- src/com/controller/SignupServlet.java
+++ src/com/controller/SignupServlet.java
@@ -2,6 +2,7 @@ package com.controller;
 
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -16,6 +17,39 @@ public class SignupServlet extends HttpServlet {
 	protected void service(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		System.out.println("SignupServlet");
-		
+
+		// read
+		String email = request.getParameter("email");// String
+		String firstName = request.getParameter("firstName");
+		String password = request.getParameter("password");
+		// validation
+		boolean isError = false;
+		if (email == null || email.trim().length() == 0) {
+			isError = true;
+		}
+		if (firstName == null || firstName.trim().length() == 0) {
+			isError = true;
+		}
+
+		if (password == null || password.trim().length() == 0) {
+			isError = true;
+		}
+
+		// data
+		// duplicate
+		// save
+		// response
+
+		if (isError == true) {
+			// goback
+			System.out.println("ERROR");
+			RequestDispatcher rd = request.getRequestDispatcher("Signup.html");
+			rd.forward(request, response);
+		} else {
+			System.out.println("SUCCESS");
+			RequestDispatcher rd = request.getRequestDispatcher("Login.html");
+			rd.forward(request, response);
+		}
+
 	}
 }
